6fe76d33eb5262e893e3a23d28523c88165995d1
KARTHIK KAVALI <karthikkavali03@gmail.com>
2026-08-05 00:35:18 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (52.64%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -3,12 +3,11 @@ class Solution {
         int count = 0;
         int i = s.length() - 1;
 
-        // Skip trailing spaces
+        
         while (i >= 0 && s.charAt(i) == ' ') {
             i--;
         }
 
-        // Count characters of the last word
         while (i >= 0 && s.charAt(i) != ' ') {
             count++;
             i--;
